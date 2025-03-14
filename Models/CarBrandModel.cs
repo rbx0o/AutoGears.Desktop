acd2ddfbe0918468959805e6df786b3d0d5a6d6e
rbx <none@none.none>
2025-03-14 17:26:54 +0300

    Создал проект

---
+++ Models/CarBrandModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace AutoGears.Models
+{
+    [Table("car_brand_models")]
+    public class CarBrandModel : BaseModel
+    {
+        [PrimaryKey("id")]
+        public Guid Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Column("car_brand_id")]
+        public Guid CarBrandId { get; set; }
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AutoGears.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
